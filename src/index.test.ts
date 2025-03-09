d273a1a8504d437e3c4920671fbb39d04113f302
Máni Eiðsson <mfe5@hi.is>
2025-03-09 05:14:20 +0000

    blah

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,10 @@
-// Simple placeholder test file
+// Simple placeholder test file using Node.js built-in test runner
 import app from './index.js';
 
-// No tests for now - this is just to make the build pass
-console.log('App is defined:', !!app);
+// Basic test to verify app exists
+import { test } from 'node:test';
+import assert from 'node:assert';
+
+test('app should be defined', () => {
+  assert.ok(app, 'App is defined');
+});
